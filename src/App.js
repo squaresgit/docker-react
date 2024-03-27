713d13cdc2c2a5fc7574a257bcf453e5de0680af
squaresgit <tatoomark@outlook.com>
2024-03-26 20:35:13 -0700

    new user and BE env created

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. change to force rebuild
+          Edit <code>src/App.js</code> and save to reload. new user and env fx
         </p>
         <a
           className="App-link"
