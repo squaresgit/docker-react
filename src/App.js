4a6650dee3c748ae8012b4dd7b4b1939b15966f3
squaresgit <tatoomark@outlook.com>
2024-04-01 19:10:40 -0700

    Removed the lock JSON package file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. new user and env fx
+          Edit <code>src/App.js</code> Remove packagoloskJSON file. Attempt
         </p>
         <a
           className="App-link"
